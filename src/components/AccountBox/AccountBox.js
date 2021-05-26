b41da286114e395f202746050a24dbecd2613dc7
Jennifer Watanabe <jwatanabe@washlaundry.com>
2021-05-26 04:39:18 -0700

    authorize github login

--- src/components/AccountBox/AccountBox.js
+++ src/components/AccountBox/AccountBox.js
@@ -3,11 +3,29 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
+import { authorize } from 'react-native-app-auth';
+import { CLIENT_ID, CLIENT_SECRET } from '@env';
 import styles from './styles';
 
 const AccountBox = ({ account }) => {
+  const config = {
+    redirectUrl: 'com.reactnativestarterkit://oauthredirect',
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    scopes: ['identity'],
+    additionalHeaders: { Accept: 'application/json' },
+    serviceConfiguration: {
+      authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+      tokenEndpoint: 'https://github.com/login/oauth/access_token',
+      revocationEndpoint:
+        `https://github.com/settings/connections/applications/${CLIENT_ID}`,
+    },
+  };
+
   const onPress = () => {
-    console.log('go to ', account.url)
+    console.log('go to ', account.url);
+
+    authorize(config).then((response) => console.log(response));
   };
 
   return (
