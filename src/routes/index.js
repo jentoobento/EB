f27177ac7e4b8748e1cba0a9d319d11691b222d5
Jennifer Watanabe <jwatanabe@washlaundry.com>
2021-05-26 02:39:29 -0700

    clean up routes, point to Home

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,51 +1,40 @@
 import React from 'react';
-import { Scene, Tabs, Stack } from 'react-native-router-flux';
+import { Scene, Stack } from 'react-native-router-flux';
 import { Icon } from 'native-base';
 import DefaultProps from '../constants/navigation';
-import AppConfig from '../constants/config';
 
 import { ArticlesForm, ArticlesList, ArticlesSingle } from '../containers';
 
-import AboutComponent from '../components/About';
+import Home from '../components/Home/Home';
 
 const Index = (
   <Stack hideNavBar>
     <Scene hideNavBar>
-      <Tabs
-        key="tabbar"
-        swipeEnabled
-        type="replace"
-        showLabel={false}
-        {...DefaultProps.tabProps}
+      <Stack
+        key="home"
+        title="Sign in to your account"
       >
-        <Stack
-          key="home"
-          title={AppConfig.appName}
-          icon={() => <Icon name="planet" {...DefaultProps.icons} />}
-          {...DefaultProps.navbarProps}
-        >
-          <Scene key="home" component={AboutComponent} />
-        </Stack>
+        <Scene key="home" component={Home} />
+      </Stack>
 
-        <Stack
-          key="articlesList"
-          title="Articles List"
-          icon={() => <Icon name="list" {...DefaultProps.icons} />}
-          {...DefaultProps.navbarProps}
-        >
-          <Scene key="articlesList" component={ArticlesList} />
-          <Scene key="articlesSingle" component={ArticlesSingle} back />
-        </Stack>
+      <Stack
+        key="articlesList"
+        title="Articles List"
+        icon={() => <Icon name="list" {...DefaultProps.icons} />}
+        {...DefaultProps.navbarProps}
+      >
+        <Scene key="articlesList" component={ArticlesList} />
+        <Scene key="articlesSingle" component={ArticlesSingle} back />
+      </Stack>
 
-        <Stack
-          key="form"
-          title="Articles Form"
-          icon={() => <Icon name="add" {...DefaultProps.icons} />}
-          {...DefaultProps.navbarProps}
-        >
-          <Scene key="form" component={ArticlesForm} />
-        </Stack>
-      </Tabs>
+      <Stack
+        key="form"
+        title="Articles Form"
+        icon={() => <Icon name="add" {...DefaultProps.icons} />}
+        {...DefaultProps.navbarProps}
+      >
+        <Scene key="form" component={ArticlesForm} />
+      </Stack>
     </Scene>
   </Stack>
 );
