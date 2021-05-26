14602e2e0850335fb2ceff5b7bdf1cfa4e807e44
Jennifer Watanabe <jwatanabe@washlaundry.com>
2021-05-26 07:24:42 -0700

    pass navigation prop to AccountBox

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Container, Content } from 'native-base';
-import {
-  ScrollView,
-} from 'react-native';
+import { ScrollView } from 'react-native';
 import { useSelector } from 'react-redux';
 import AccountBox from '../AccountBox/AccountBox';
 import styles from './styles';
 
-const Home = () => {
+const Home = ({ navigation }) => {
   const accounts = useSelector((state) => state.articles.accounts);
 
   return (
@@ -15,7 +13,11 @@ const Home = () => {
       <Content padder>
         <ScrollView contentContainerStyle={styles.scrollContent} style={styles.scoll}>
           {accounts.map((account) => (
-            <AccountBox key={account.name} account={account} />
+            <AccountBox
+              key={account.name}
+              account={account}
+              navigation={navigation}
+            />
           ))}
         </ScrollView>
       </Content>
