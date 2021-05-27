1a2825b4a096cdac0d2c86084ee1dd90a25fe332
Jennifer Watanabe <jwatanabe@washlaundry.com>
2021-05-26 18:05:51 -0700

    add logoutIcon to ListView header

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Scene, Stack } from 'react-native-router-flux';
-
+import LogoutIcon from '../components/UI/LogoutIcon';
 import Home from '../components/Home/Home';
 import ListView from '../components/ListView/ListView';
 
@@ -10,8 +10,15 @@ const Index = (
       <Stack key="home" title="Sign in to your account">
         <Scene key="home" component={Home} />
       </Stack>
-      <Stack key="listView" title="Your items">
-        <Scene key="listView" component={ListView} />
+      <Stack
+        key="listView"
+        renderRightButton={() => <LogoutIcon />}
+        title="Your items"
+      >
+        <Scene
+          key="listView"
+          component={ListView}
+        />
       </Stack>
     </Scene>
   </Stack>
